ab155c1f44ae8e8e76e74afab8fa000335acec80
Stephen Halter <halter73@gmail.com>
2018-12-06 16:24:09 -0800

    Repro

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -12,13 +14,50 @@ namespace anc_4422
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var runTask = CreateWebHostBuilder(args).Build().RunAsync();
+
+
+            for (var i = 0; i < 256; i++)
+            {
+                _ = RequestTestFileAndAbort();
+            }
+
+            await runTask;
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        private static async Task RequestTestFileAndAbort()
+        {
+            while (true)
+            {
+                try
+                {
+                    using (var client = new TcpClient())
+                    {
+                        await client.ConnectAsync(IPAddress.Loopback, 5000);
+
+                        var stream = client.GetStream();
+
+                        using (var writer = new StreamWriter(stream))
+                        {
+                            await writer.WriteAsync("GET /test.txt HTTP/1.1\r\nHost: localhost:5000\r\n\r\n");
+                            await writer.FlushAsync();
+
+                            await Task.Delay(5000);
+                            client.LingerState = new LingerOption(true, 0);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Excpeption: {0}", ex);
+                }
+            }
+        }
     }
 }
